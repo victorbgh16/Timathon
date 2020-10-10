a5c67c9dc3fa0e0c59e96e653ceb3ca3a287ca55
dnaprobe <72040503+dnaprobe@users.noreply.github.com>
2020-10-10 16:10:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ root = Tk()
 root.title("School Planer") 
 root.geometry('1280x720') 
 
-header = Label(root, text="The best School Planer").pack()
+header = Label(root, text="The best School Planer")
+header.grid()
 
 info = Label(root, text="Add or Remove a Lesson")
+info.grid()
 
 root.mainloop()
