3b900ccf33906d7976973ead13e96cff2705f987
victorbgh16 <71562786+victorbgh16@users.noreply.github.com>
2020-10-10 16:46:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,19 @@ def retrieve_input():
 root.mainloop()
 
 # https://stackoverflow.com/questions/14824163/how-to-get-the-input-from-the-tkinter-text-widget
+
+
+#from tkinter import *
+#root=Tk()
+#def retrieve_input():
+#    inputValue=textBox.get("1.0","end-1c")
+#    print(inputValue)
+#
+#textBox=Text(root, height=2, width=10)
+#textBox.pack()
+#buttonCommit=Button(root, height=1, width=10, text="Commit", 
+#                    command=lambda: retrieve_input())
+#command=lambda: retrieve_input() >>> just means do this when i press the button
+#buttonCommit.pack()
+#
+#mainloop()
