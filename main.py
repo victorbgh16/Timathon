2a6772961e47bbb17cd89aea2a84b9088244217f
dnaprobe <72040503+dnaprobe@users.noreply.github.com>
2020-10-10 17:01:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,32 +12,12 @@ header = Label(root, text="The best School Planer").pack()
 
 info = Label(root, text="Add or Remove a Lesson").pack()
 
-subjectInput = Text(root, height=1, width=30).pack()
+subjectInput = Text(root, height=1, width=10, text="Read", command=addButtonpressed)
 
 def addButtonpressed():
-	print('Test')
+	result=textExample.get("1.0","end")
+    	print(result)
 
 addButton = Button (root, text = 'Add', command = addButtonpressed).pack()
 
-def retrieve_input():
-	input = self.subjectInput.get('1.0', 'end-1c')
-
 root.mainloop()
-
-# https://stackoverflow.com/questions/14824163/how-to-get-the-input-from-the-tkinter-text-widget
-
-
-#from tkinter import *
-#root=Tk()
-#def retrieve_input():
-#    inputValue=textBox.get("1.0","end-1c")
-#    print(inputValue)
-#
-#textBox=Text(root, height=2, width=10)
-#textBox.pack()
-#buttonCommit=Button(root, height=1, width=10, text="Commit", 
-#                    command=lambda: retrieve_input())
-#command=lambda: retrieve_input() >>> just means do this when i press the button
-#buttonCommit.pack()
-#
-#mainloop()
