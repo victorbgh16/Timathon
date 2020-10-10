0dfd55a95991aeb150310d490c1c876f26602f00
dnaprobe <72040503+dnaprobe@users.noreply.github.com>
2020-10-10 15:44:28 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+   
+root = Tk() 
+
+root.title("School Planer") 
+root.geometry('1280x720') 
+
+header = Label(root, text = "The best School Planer") 
+header.grid() 
+
+root.mainloop()
