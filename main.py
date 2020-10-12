cccebc4dc20fdd6a717a6e81b4c92b5bf2277e5d
dnaprobe <72040503+dnaprobe@users.noreply.github.com>
2020-10-12 19:34:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,85 +1,105 @@
 from tkinter import *
 import time
 from tkinter.ttk import Notebook
-   
+
 root = Tk()
 
+
 def clock():
-	t = time.strftime('%I:%M:%S',time.localtime())
-	if t != '':
-		label1.config(text = t,font = 'times 25')
-	root.after(100,clock)
-label1 = Label(root,justify = 'center')
+    t = time.strftime('%I:%M:%S', time.localtime())
+    if t != '':
+        label1.config(text=t, font='times 25')
+    root.after(100, clock)
+
+
+label1 = Label(root, justify='center')
 label1.pack()
 clock()
 
-root.title("School Planer") 
-root.geometry('1280x720') 
+root.title("School Planer")
+root.geometry('1280x720')
 
-header = Label(root, text = "The best School Planner")
+header = Label(root, text="The best School Planner")
 header.pack()
 
-info = Label(root, text = "Add or Remove a Lesson")
+info = Label(root, text="Add or Remove a Lesson")
 info.pack()
 
-def addButtonpressed():
-	result = subjectInput.get("1.0","end")
-	print(result)
-	result_label = Label(root, text = result)
-	result_label.pack()
 
-subjectInput = Text(root, height = 1)
+def addButtonpressed():
+    result = subjectInput.get("1.0", "end")
+    print(result)
+    result_label = Label(root, text=result)
+    result_label.pack()
+
+
+subjectInput = Text(root, height=1)
 subjectInput.pack()
 
-addButton = Button(root, text = 'Add', command = addButtonpressed)
+addButton = Button(root, text='Add', command=addButtonpressed)
 addButton.pack()
 
-def RemoveButtonPressed():
-	result2 = RemoveInput.get('1.0', 'end')
-	print(result2)
 
-RemoveInput = Text(root, height = 1)
+def RemoveButtonPressed():
+    result2 = RemoveInput.get('1.0', 'end')
+    print(result2)
+
+
+RemoveInput = Text(root, height=1)
 RemoveInput.pack()
 
-RemoveButton = Button(root, text = 'Remove', command = RemoveButtonPressed)
+RemoveButton = Button(root, text='Remove', command=RemoveButtonPressed)
 RemoveButton.pack()
 
-def MathsButtonPressed():
-	print('maths')
-	maths_label = Label(root, text = 'maths')
-	maths_label.pack()
 
-MathsButton = Button(root, text = 'add maths', command = MathsButtonPressed)
+def MathsButtonPressed():
+    print('maths')
+    maths_label = Label(root, text='Maths')
+    maths_label.pack()
+
+
+MathsButton = Button(root, text='add Maths', command=MathsButtonPressed)
 MathsButton.pack()
 
+
 def EnglishButtonPressed():
-	print('english')
-	english_label = Label(root, text = 'english')
-	english_label.pack()
+    print('english')
+    english_label = Label(root, text='English')
+    english_label.pack()
 
 
-EnglishButton = Button(root, text = 'add english', command = EnglishButtonPressed)
+EnglishButton = Button(root, text='add English', command=EnglishButtonPressed)
 EnglishButton.pack()
 
+
+
+def BreakButtonPressed():
+    print('break')
+    break_label = Label(root, text='Break')
+    break_label.pack()
+
+
+BreakButton = Button(root, text='add a Break', command=BreakButtonPressed)
+BreakButton.pack()
+
 frame = Frame(root)
-frame.pack(fill = 'both')
+frame.pack(fill='both')
 
 tablayout = Notebook(frame)
 
 tab = Frame(tablayout)
-tab.pack(fill = 'both')
+tab.pack(fill='both')
 
-label = Label(tab, text = 'this is just a test')
+label = Label(tab, text='this is just a test')
 label.pack()
-tablayout.add(tab, text = 'TAB 1')
+tablayout.add(tab, text='TAB 1')
 
 tab2 = Frame(tablayout)
-tab2.pack(fill = 'both')
-label2 = Label(tab2, text = 'this is the second test')
+tab2.pack(fill='both')
+label2 = Label(tab2, text='this is the second test')
 label2.pack()
-tablayout.add(tab2, text = 'TAB 2')
+tablayout.add(tab2, text='TAB 2')
 
-tablayout.pack(fill = 'both')
+tablayout.pack(fill='both')
 
 root.mainloop()
-
